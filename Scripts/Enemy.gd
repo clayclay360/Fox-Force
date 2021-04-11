66921e874eee801dadcc211ce84424d759cfbb83
Clay <38699310+clayclay360@users.noreply.github.com>
2021-04-11 17:10:52 -0400

    Update Enemy.gd

--- Scripts/Enemy.gd
+++ Scripts/Enemy.gd
@@ -10,19 +10,19 @@ func _ready():
 	pass # Replace with function body.
 
 func _physics_process(delta):
-	$Sprite.flip_h = velocity.x > 0
+	$Sprite.flip_h = velocity.x > 0  # flip the sprite if the velocity x is greater than zero
 	velocity.y += gravity * delta # decrease y velocity by gravity times delta 
-	velocity.x = facing * speed
-	velocity = move_and_slide(velocity, Vector2(0,-1))
+	velocity.x = facing * speed # set velocity x to facing multiplied by speed 
+	velocity = move_and_slide(velocity, Vector2(0,-1)) # set velocity to move along the velocity flip it's direction along the y axis
 	
 	for index in range(get_slide_count()):
-		var collision = get_slide_collision(index)
+		var collision = get_slide_collision(index) # get an array of all the collision of the enemy
 		if collision.collider.name == "Player":
-			collision.collider.hurt()
+			collision.collider.hurt() # run the hurt function
 		if collision.normal.x != 0:
-			facing = sign(collision.normal.x)
+			facing = sign(collision.normal.x) # flip the direction the enemy is facing
 		if position.y > 1000:
-			queue_free()
+			queue_free() # destroy enemy
 
 func take_damage():
 	$AnimationPlayer.play("death") # play death animation
@@ -32,4 +32,4 @@ func take_damage():
 		
 func _on_AnimationPlayer_animation_finished(anim_name):
 	if anim_name == "death":
-		queue_free()
+		queue_free() # destroy enemy
