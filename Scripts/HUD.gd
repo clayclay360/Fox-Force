d31a5cda6787f87e8f2827e087a28d3218153f83
Clay <38699310+clayclay360@users.noreply.github.com>
2021-04-11 17:14:20 -0400

    Update HUD.gd

--- Scripts/HUD.gd
+++ Scripts/HUD.gd
@@ -6,9 +6,9 @@ onready var life_counter = [$HBoxContainer/HBoxContainer2/L1, $HBoxContainer/HBo
 func _on_Player_life_changed(value):
 	for heart in range(life_counter.size()):
 		if value - heart <= 0:
-			life_counter[heart].visible = false
+			life_counter[heart].visible = false # set to false
 		
 		
 func _on_Score_changed(value):
-	$HBoxContainer/ScoreLabel.text = str(value)
+	$HBoxContainer/ScoreLabel.text = str(value) # set the score label text to equal the value (covert to string)
 
