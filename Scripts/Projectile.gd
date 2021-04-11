72ccd3515aa50f89ced7632a15446b372896b1e1
Clay <38699310+clayclay360@users.noreply.github.com>
2021-04-11 17:18:41 -0400

    Update Projectile.gd

--- Scripts/Projectile.gd
+++ Scripts/Projectile.gd
@@ -3,14 +3,14 @@ extends Area2D
 export (float) var speed
 
 func _physics_process(delta):
-	position.x += speed * delta
+	position.x += speed * delta # increase the position x by speed multiplied by delta
 	
-
+# this function is called when the acorn hit's another node's body collision
 func _on_Acorn_body_entered(body):
 	if body.is_in_group("enemy"):
-		body.take_damage()
-		queue_free()
+		body.take_damage() # run the take damage function
+		queue_free() # destroy projectile
 	elif body.is_in_group("player"):
 		return
 	else:
-		queue_free()
+		queue_free() # destroy projectile 
