2d0cd63707e26a553ab90245150652d9da5e134b
Clay <38699310+clayclay360@users.noreply.github.com>
2021-04-11 17:06:32 -0400

    Update Collectible.gd

--- Scripts/Collectible.gd
+++ Scripts/Collectible.gd
@@ -11,13 +11,14 @@ func _ready():
 	pass # Replace with function body.
 
 func init(type, pos):
-	$Sprite.texture = load(textures[type])
-	position = pos
+	$Sprite.texture = load(textures[type]) # change the sprite texture to the loaded texture
+	position = pos # set position to pos
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 
 
 func _on_Collectible_body_entered(body):
-	emit_signal("pickup")
-	queue_free()
+	emit_signal("pickup") # emit pick up signal
+	queue_free() # destroy collectible
