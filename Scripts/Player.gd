4cda38633cbf2f207370caf3c873bd89afd79bb9
Clay <38699310+clayclay360@users.noreply.github.com>
2021-04-11 17:04:47 -0400

    Update Player.gd

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -91,10 +91,10 @@ func _physics_process(delta):
 				collision.collider.take_damage() # run take damage function from collider
 				velocity.y = - 200 # decrease by 200
 			else:
-				hurt()	
+				hurt()	# run the hurt function
 		if collision.collider.is_in_group("climbable"):
-			wall_is_climbable = true
-			print("climbing")
+			wall_is_climbable = true # set to true
+			print("climbing") # print is climbling
 	if position.y > 1000:
 		change_state(DEAD) # change state to dead
 	
@@ -106,17 +106,17 @@ func get_input():
 	var left = Input.is_action_pressed("left") # get left input
 	var jump = Input.is_action_just_pressed("jump") # get jump input
 	var throw = Input.is_action_just_pressed("throw") # get throw input
-	var up = Input.is_action_pressed("up")
-	var down = Input.is_action_pressed("down")
+	var up = Input.is_action_pressed("up") # get the up input
+	var down = Input.is_action_pressed("down") # get the down input
 	
 	if right:
 		velocity.x += run_speed # increase x velocity by run speed
 		$Sprite.flip_h = false # set false
-		direction = 1
+		direction = 1 # set to 1
 	if left:
 		velocity.x -= run_speed # decrease velocity by run speed
 		$Sprite.flip_h = true # set true
-		direction = -1
+		direction = -1 # set to -1
 	if jump and is_on_floor():	
 		change_state(JUMP) #change state to jump
 		velocity.y = jump_speed # equal jump speed
@@ -131,38 +131,38 @@ func get_input():
 	if state == IDLE and !is_on_floor() || state == RUN and !is_on_floor():
 		change_state(JUMP) #change state to jump
 	if state == IDLE and throw:
-		var particle_instance = acorn.instance()
-		get_tree().current_scene.add_child(particle_instance)
-		particle_instance.global_transform = $ProjectileSpawn.global_transform
-		particle_instance.speed *= direction
+		var particle_instance = acorn.instance() # create acorn instance
+		get_tree().current_scene.add_child(particle_instance) # load acon instance to scene 
+		particle_instance.global_transform = $ProjectileSpawn.global_transform # instance global transform equals projectile spawn global transform
+		particle_instance.speed *= direction # multiply speed by direction
 	if is_climbing and up:
-		velocity.y -= climbing_speed
-		clamp(velocity.y,0,climbing_speed)
+		velocity.y -= climbing_speed # decrease the velocity y by climbing speed
+		clamp(velocity.y,0,climbing_speed) # clamp the velocity y by 0 and climbing_speed 
 	if is_climbing and down:
-		velocity.y += climbing_speed
-		clamp(velocity.y,0,climbing_speed)
+		velocity.y += climbing_speed # increase the velocity y by climbing_speed
+		clamp(velocity.y,0,climbing_speed) # clamp the velocity y by 0 as the minimum and climbing_speed as the max
 	if is_on_wall() and wall_is_climbable:
-		is_climbing = true
-		is_hugging = true
-		velocity.y = 0
+		is_climbing = true # set to true
+		is_hugging = true # set to true
+		velocity.y = 0 # set to zero
 	elif is_hugging and !is_on_wall():
-		is_climbing = false
-		is_hugging = false
-		wall_is_climbable = false
+		is_climbing = false # set to false
+		is_hugging = false # set to false
+		wall_is_climbable = false # set to false
 	if Input.is_action_just_pressed("right") and press_count == 0:
-		press_count = 1
-		$Timer.start()
+		press_count = 1 # set to 1
+		$Timer.start() # start timer 
 	elif Input.is_action_just_pressed("right") and press_count == 1 and $Timer.time_left > 0:
 		print("double press")
-		press_count = 0
-		velocity.x = dash_speed
+		press_count = 0 # set to zero
+		velocity.x = dash_speed # set to dash speed
 	if Input.is_action_just_pressed("left") and press_count == 0:
-		press_count = 1
-		$Timer.start()
+		press_count = 1 # set to 1
+		$Timer.start() # start timer
 	elif Input.is_action_just_pressed("left") and press_count == 1 and $Timer.time_left > 0:
 		print("double press")
 		press_count = 0
-		velocity.x = -dash_speed
+		velocity.x = -dash_speed # subtract velocity.x by dash_speed
 	
 func hurt():
 	if state != HURT:
@@ -171,10 +171,10 @@ func hurt():
 func climb():
 	is_climbing = ! is_climbing
 	if is_climbing:
-		change_state(CLIMB)
+		change_state(CLIMB) # change state to climb
 	else:
-		change_state(RUN)
+		change_state(RUN) # change state to run
 
 
 func _on_Timer_timeout():
-	press_count = 0
+	press_count = 0 # set to zero
