e62020748df231c11ac819ba83a4df00b2642dc9
Clay <38699310+clayclay360@users.noreply.github.com>
2021-04-11 17:15:38 -0400

    Update Ladder.gd

--- Scripts/Ladder.gd
+++ Scripts/Ladder.gd
@@ -1,10 +1,11 @@
 extends Area2D
 
+# this function is called when a node has entered the area 2D
 func _on_Ladder_body_entered(body):
 	if body.name == "Player":
 			body.climb() # run the climb function
 
-
+# this function is called when a node has exited the area 2D
 func _on_Ladder_body_exited(body):
 	if body.name == "Player":
 			body.climb() # run the climb function 
